270206de32e6e03e13997a6296f839e9e685961d
tayyab <tayyabnaveed5341@gmail.com>
2022-12-14 12:26:36 +0500

    new task initial indexing

--- lib/Task.py
+++ lib/Task.py
@@ -3,35 +3,50 @@ from lib.common import Common
 from lib.Paths import Path
 import os
 from time import sleep
-
+import shutil
 
 class Task:
     DESTINATION_PATH: object
     INITIAL_DIRECTORY_NAME = "initial_list.d"
     ATTEMPTS_DIRECTORY_NAME = "attempts.d"
 
-    def __init__(self, name, source, destination):
+    def __init__(self, name, source="", destination=""):
         self.DESTINATION_PATH = destination
         self.name = name
         self.full_path = Path.generate_task_path(self.name)
         self.source_path = source
 
+        #if(False):  # check of the task already exists
+            #   TODO: resume
+        #else:
         self.create_new()
 
+    def abort_creation(self, message: str):
+        print(message, '\nrolling back')
+        shutil.rmtree(self.full_path)
+        exit()
+
     def create_new(self):
+        config_file_path: str = self.full_path + '/config'
+        print('checking if the task already exists')
         if Task.exists(self.full_path):
-            print(Common.json_file('config'))
-            exit('there\'s already a task with that name\nPlease choose a different name')
+            print('there\'s already a task with that name\nPlease choose a different name')
+            exit(Common.json_file(config_file_path))
         try:
             os.makedirs(self.full_path)
             os.makedirs(Path.valid_path(self.full_path, self.INITIAL_DIRECTORY_NAME))
         except OSError as e:
             print('m16')
             exit(e)
-        Common.json_file('config', {
-            "source_path": self.source_path,
-            "destination_path": self.DESTINATION_PATH
-        })
+        try:
+            Common.json_file(config_file_path, {
+                "source_path": self.source_path,
+                "destination_path": self.DESTINATION_PATH
+            })
+        except BaseException as ex:
+            print(ex)
+            print(ex.__class__)
+            self.abort_creation('XT~39 unhandled exception')
 
     def generate_initial_list(self):
         if os.path.isdir(Path.valid_path(self.full_path, self.ATTEMPTS_DIRECTORY_NAME)):
@@ -53,6 +68,7 @@ class Task:
                     init['remaining'].append(path + "/")
                 elif os.path.isfile(path):
                     init['files'].append(path)
+        save_point_service.backup()
         del save_point_service
 
     @staticmethod
--- lib/common.py
+++ lib/common.py
@@ -1,14 +1,26 @@
 import json
+from multipledispatch import dispatch
 
 
 class Common:
     @staticmethod
+    @dispatch(str)
     def json_file(file_name: str) -> dict or list or tuple:
-        with open(file_name + ".json", "r") as outfile:
-            return json.load(outfile.read(file_name))
+        try:
+            with open(file_name + ".json", "r") as outfile:
+                print(outfile.__class__)
+                print(outfile.read())
+                return json.load(outfile.read())
+        except FileNotFoundError:
+            exit('Xc~13 task config not found')
+        except Exception as bx:
+            print(bx)
+            #print(bx.with_traceback(bx))
+            exit('Xc~15 unhandled exception')
 
     @staticmethod
-    def json_file(file_name: str, jsonable: dict, mode_append=False):
+    @dispatch(str, dict, mode_append=bool)
+    def json_file(file_name: str, jsonable: dict, mode_append: bool = False):
         with open(file_name + ".json", "a" if mode_append else "w") as outfile:
             json.dump(jsonable, outfile)
             # return outfile.write(jsonable)
